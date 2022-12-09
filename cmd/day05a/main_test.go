630e74a17c8e92d03466018f1fa7d98a7c18cabd
Scott Walker <scott.walker@harness.io>
2022-12-09 15:21:25 +0000

    Day 05a

---
+++ cmd/day05a/main_test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+//     [D]
+// [N] [C]
+// [Z] [M] [P]
+//  1   2   3
+
+// move 1 from 2 to 1
+// move 3 from 1 to 3
+// move 2 from 2 to 1
+// move 1 from 1 to 2
+
+func TestProcess(t *testing.T) {
+	stacks := [][]byte{
+		{'Z', 'N'},
+		{'M', 'C', 'D'},
+		{'P'},
+	}
+	directions := []string{
+		"move 1 from 2 to 1",
+		"move 3 from 1 to 3",
+		"move 2 from 2 to 1",
+		"move 1 from 1 to 2",
+	}
+	assert.Equal(t, "CMZ", process(stacks, directions))
+}
